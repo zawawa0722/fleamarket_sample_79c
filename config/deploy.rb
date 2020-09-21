a43e1ba954b22deea28ab78f3e811248a533b6e0
zawawa0722 <sekaoza1213@gmail.com>
2020-09-21 10:21:21 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.14.1'
 
 # 自身のアプリ名、ユーザー名、リポジトリ名を記述
 set :application, 'fleamarket_sample_79c'
-set :repo_url,  'git@github.com:githubのユーザー名/リポジトリ名.git'
+set :repo_url,  'git@github.com:zawawa0722/fleamarket_sample_79c.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.6.5'
 
 # chat-spaceで使ったpemを指定
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/xxx.pem']
+                  keys: ['~/.ssh/chat-space.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
