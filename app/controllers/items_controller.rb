f534e2cab82556de9621d0525deb21a2599b1a80
zawawa0722 <sekaoza1213@gmail.com>
2020-10-13 00:21:29 +0900

    create pull request

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,17 @@
 class ItemsController < ApplicationController
-  def index; end
+
+  def index
+    @items = Item.all
+    @images = Image.all
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
 end
