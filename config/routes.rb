0b41d5787d2a2e90bc7bf576ceb8d7d6bfea9286
hibinyo1031 <68359380+hibinyo1031@users.noreply.github.com>
2020-10-17 18:39:25 +0900

    detail commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'front#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "front#index"
+  resources :items
+  get 'items/index'
+  get 'detail/index'
 end
