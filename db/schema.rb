3e3386f891790ed49d2307f6bc0715d6bbe0fbc1
damian-neel <68770222+damian-neel@users.noreply.github.com>
2020-10-18 16:33:22 +0900

    sign

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,14 @@
 ActiveRecord::Schema.define(version: 2020_10_11_030422) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
