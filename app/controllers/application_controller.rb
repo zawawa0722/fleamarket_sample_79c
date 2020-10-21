15e6baa03f3d12278782ec9af5cf0f4d2ee87f2d
damian-neel <68770222+damian-neel@users.noreply.github.com>
2020-10-21 16:39:14 +0900

    wizz_wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,13 @@ class ApplicationController < ActionController::Base
   end
 
   def confingure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname,:family_name,:first_name,:family_name_kana,:first_name_kana,:birthday,])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [
+      :nickname,
+      :family_name,
+      :first_name,
+      :family_name_kana,
+      :first_name_kana,:birthday,
+      ])
   end
 
   def basic_auth
--- db/migrate/20201020094343_create_addresses.rb
+++ db/migrate/20201020094343_create_addresses.rb
@@ -1,7 +1,9 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.integer :zipcode
+      t.text :address
+      t.references :user
       t.timestamps
     end
   end
