f7c018d7946577297f8eec9508f4ef3d40e7c467
damian-neel <68770222+damian-neel@users.noreply.github.com>
2020-10-24 12:48:30 +0900

    shimizu

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
+  validates :nickname, :nickname, :family_name, :first_name, :family_name_kana, 
+      :first_name_kana, :birthday, :zipcode, :prefectures, :city, :address, :building,
+      :address_name1,:address_name2, :phone, presence: true
   has_one :address
 end
--- db/migrate/20201020094343_create_addresses.rb
+++ db/migrate/20201020094343_create_addresses.rb
@@ -2,7 +2,13 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.integer :zipcode
+      t.text :prefectures
+      t.text :city
       t.text :address
+      t.text :building
+      t.text :address_name1
+      t.text :address_name2
+      t.text :phone
       t.references :user
       t.timestamps
     end
