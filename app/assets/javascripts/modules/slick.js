1852643e686d9b6279db1f968141ac8e387d6d00
YourName <you@example.com>
2020-11-21 17:58:56 +0900

    新規商品の表示方法変更

---
+++ app/assets/javascripts/modules/slick.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('.Pickup__Contents__Lists').slick({
+    slidesToShow: 5,
+    prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
+    nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>'
+  }); 
+});
